564c074537d43de6c149525d136e536bee10f46e
"Raghavendra Idli" <"raghavendra@frontier.xyz">
2022-04-07 09:34:01 +0530

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Vesting Contract
+
+Token Link -> https://kovan.etherscan.io/address/0x466e68991981d920a1a6129d5b93d9f2c1fdf430#code
+
+Token Vesting Link -> https://kovan.etherscan.io/address/0x4c93ea90823c834b5bf410d99f31a8afe28bd23c#code
---
+++ contracts/RToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract Token is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+}
